861f3b610750299015ea0e3885fca6c214a52c6c
AnaiLyu <yuliana2221@gmail.com>
2019-03-14 00:57:45 +0200

    added script 'temperature'

---
+++ script.js
@@ -0,0 +1,3 @@
+let tC = +prompt('Enter the temperature by Celsius and get converted to temperature by Fahrenheit');
+let tF = tC * (9/5) + 32
+alert('You entered the temperature by Celsius ' + tC + ', and when translated to Fahrenheit is ' + tF);
